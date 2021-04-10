f8490052173679448849d1451f7232b1a0b2bda6
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-10 16:14:18 -0700

    Update multiplier.v
    not sure if this works yet, will work on testbench and commit once it passes tests

--- multiplier.v
+++ multiplier.v
@@ -1,7 +1,19 @@
-module multiplier(input  [31:0] a,b,
-		  input clk,start,is_signed,
+module multiplier(input  [31:0] a, b,
+		  input Clk, start, Is_signed,
 		  output [63:0] s);
-
-  //[TODO:] Multiplier Implementation
+	
+	//lab says to assume no difference between signed and unsigned instructions
+	
+	//Multiplier Implementation
+	always @(posedge Clk) begin
+		if (start) begin
+			//lab says to assume no difference between signed and unsigned instructions
+			if (Is_signed) begin
+				s <= a * b;
+			end else begin
+				s <= a * b;
+			end
+		end
+	end
 
 endmodule
