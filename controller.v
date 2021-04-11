df6dff64085cdfe7ef4855b17494ad26530d87fe
Srikar Arani <58278617+srikar-arani@users.noreply.github.com>
2021-04-10 20:35:58 -0700

    controller change

--- controller.v
+++ controller.v
@@ -1,28 +1,56 @@
 module controller(input   [5:0] op, func,
                   input         eq_ne,
-                  output        memtoreg, memwrite,
-                  output        memread, regwrite,
+                  output        memwrite, memread,
+                  output        regwrite,
                   output        alusrcA, alusrcB,
-		  output	se_ze, reg_dst,
+		  output	se_ze, regdst,
                   output        start_mult, mult_sign,
+		  output 	memtoreg,
                   output  [1:0] pc_source, out_select,
 		  output  [3:0] alu_op,
 		  output	output_branch);
 
-//[TODO:] Insert Zero Wire
-wire[1:0] aluop;
+  //[TODO:] Insert Zero Wire
 
-/********************************************************
-This from Lab 4, it all needs to change
-*********************************************************/
-wire branch, bne;
+  wire branch;
 
-//maindec md(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, bne, ori, aluop);
-//aludec ad(func, aluop, alucontrol);
+  maindec md(op, memwrite, memread, regwrite, alusrcA, alusrcB, se_ze, regdst, start_mult, mult_sign, memtoreg, out_select, alu_op);
 
-//assign pc_source = (branch & zero) || (bne & !zero);
+  
+
+  //assign pc_source = (branch & zero) || (bne & !zero);
 
 
 
+
+endmodule
+
+module maindec(input  [5:0] op,
+               output 	    memwrite, memread,
+	       output 	    regwrite,
+	       output 	    alusrcA, alusrcB,
+	       output 	    se_ze, regdst,
+               output	    start_mult, mult_sign,
+	       output	    memtoreg,
+               output [1:0] out_select,
+	       output [3:0] alu_op);
+
+  reg [15:0] controls;
+
+  assign {memwrite, memread, regwrite, alusrcA, alusrcB, se_ze, regdst, start_mult, mult_sign, memtoreg, out_select, alu_op} = controls;
+
+  always @ * begin
+    case (op)
+      6'b000000: controls <= 16'b0010001000xxxxxx; // RTYPE
+      6'b100011: controls <= 16'b101001000xx0010; // LW
+      6'b101011: controls <= 16'b001010000xx0010; // SW
+      6'b000100: controls <= 16'b000100000xx1010; // BEQ
+      6'b001000: controls <= 16'b101000000xx0010; // ADDI
+      6'b000010: controls <= 16'b000000100xx0010; // J
+      6'b001101: controls <= 16'b101000001xx0001; // ORI
+      6'b000101: controls <= 16'b000100010xx1010; // BNE
+      default: controls <= 16'bxxxxxxxxxxxxxxxx; // illegal op
+    endcase
+  end
 
 endmodule
--- reg_file.v
+++ reg_file.v
@@ -1,23 +1,22 @@
-module regfile(input Clk,
-	       input Write,
-	       input Reset,
-	       input [4:0] PR1, PR2, WR,
-	       input [31:0] WD,
-	       output [31:0] RD1, RD2);
+module regfile(input clk,
+	       input write,
+	       input reset,
+	       input [4:0] pr1, pr2, wr,
+	       input [31:0] wd,
+	       output [31:0] rd1, rd2);
 
-	reg [31:0] rf[31:0];
+  reg [31:0] rf[31:0];
 
-  
-  // Reset Values
-	assign RD1 = 0;
-	assign RD2 = 0;
+  integer i;
 
-	always @(posedge Clk, posedge Reset) begin
-    // Only assign if Reset is 0 
-		if (Write & !Reset) rf[WR] <= WD;
-	end
-	
-	assign RD1 = (PR1 != 0) ? rf[PR1] : 0;
-	assign RD2 = (PR2 != 0) ? rf[PR2] : 0;
+  always @(posedge clk) begin
+    if (reset) begin
+      for (i = 0; i < 31; i=i+1) rf[i] = 0;
+    end
+    else if (write) rf[wr] <= wd;
+  end
+
+  assign rd1 = (pr1 != 0) ? rf[pr1] : 0;
+  assign rd2 = (pr2 != 0) ? rf[pr2] : 0;
 
 endmodule
