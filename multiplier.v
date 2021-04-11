99b83a9b4d8324256740905eb2f8666f7dee9ada
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-10 17:22:14 -0700

    Update multiplier.v
    this implementation passes testbench

--- multiplier.v
+++ multiplier.v
@@ -1,7 +1,19 @@
-module multiplier(input  [31:0] a,b,
-		  input clk,start,is_signed,
-		  output [63:0] s);
+module multiplier(input  [31:0] a, b,
+		  input Clk, start, Is_signed,
+		  output reg [63:0] s);
 
-  //[TODO:] Multiplier Implementation
+    //value to hold result of multiplication
+    wire [63:0] M;
+
+    assign M = a * b;
+	
+    //multiplier Implementation
+    always @ (posedge Clk) begin
+        case(Is_signed)
+            //lab says to assume no difference between signed and unsigned instructions
+            1'b0 : s <= M;
+            1'b1 : s <= M;
+        endcase
+    end
 
 endmodule
