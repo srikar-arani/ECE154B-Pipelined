aa2225a85639dcb51b7657a799fff287e32e5d33
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-10 17:18:54 -0700

    Update multiplier_tb.v
    Test passes on modelsim

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -1,17 +1,34 @@
-module multiplier_tb;
-  reg clk,start,is_signed;
+module Multiplier_tb;
+  reg clk, start,is_signed;
   reg [31:0] a,b;
   wire [63:0] s;
 
-  multiplier multiplier_test (
+  Multiplier multiplier_test(
     .a(a),
     .b(b),
-    .clk(clk),
+    .Clk(clk),
     .start(start),
-    .is_signed(is_signed),
+    .Is_signed(is_signed),
     .s(s)
     );
-
-  //[TODO:] multiplier testbench
+  
+  initial begin
+    assign a = 5;
+    assign b = 5;
+    assign is_signed = 0;
+    clk = 1;
+    #10;
+      
+    if (s == 25) begin
+        $display("PASS");
+        #5;
+        $stop;
+    end else begin
+        $display("FAIL");
+        #5;
+        $stop;
+    end
+    
+  end
 
 endmodule
