3f7ae36b81a03b59c00067628e9ed188d462fe73
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-11 18:53:50 -0700

    Update inst_memory.v
    Passes testbench

--- inst_memory.v
+++ inst_memory.v
@@ -1,17 +1,13 @@
 module inst_memory(input [31:0]  address,
             	   output [31:0] read_data);
 
-
-
-  //[TODO:]This module needs to be updated. Not sure how to actually do this, so it would be good to take a look
-
   reg [31:0] RAM[63:0];
 
   initial
     begin
-      $readmemh("memfile.dat",RAM); // initialize memory with test program. Change this with memfile2.dat for the modified code
+      $readmemh("memfile.dat",RAM); // initialize memory with test program
     end
 
-  assign read_data = RAM[address[7:2]]; // word aligned
+  assign read_data = RAM[address[31:2]]; // word aligned
 
 endmodule
