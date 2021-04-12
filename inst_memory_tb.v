eb5488afa463ffb1a7162b233305ca0805d5c5ca
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-11 18:55:24 -0700

    Update inst_memory_tb.v
    this testbench gives success results

--- inst_memory_tb.v
+++ inst_memory_tb.v
@@ -1,36 +1,26 @@
-module imem_tb;
-  reg [31:0] address;
-  wire [31:0] read_data;
-
-  inst_memory imem_test (
+module inst_memory_tb();
+	reg [31:0] address;
+	wire [31:0] read_data;
+	
+	inst_memory imem_test (
     .address(address),
     .read_data(read_data)
   );
 
-  //[TODO:] inst_memory testbench
-
   integer i;
 
-  initial
-    begin
-      assign address = 8'h00000000;
-      #20;
-      assign address = 8'h00000004;
-      #20;
-      assign address = 8'h00000008;
-      #20;
-      assign address = 8'h0000000C;
-      #20;
-      assign address = 8'h00000010;
-      #20;
-      assign address = 8'h00000014;
-      #20;
-      assign address = 8'h00000018;
-      #20;
-      assign address = 8'h0000001C;
-      #20;
-      assign address = 8'h00000020;
-      #20;
-    end
+	initial begin
+		assign address = 8'h00000000;
+		#10;
+		if (read_data == 8'h00000000) begin
+			$display("PASS");
+			#5;
+			$stop;
+		end else begin
+			$display("FAIL");
+			#5;
+			$stop;
+		end
+	end
 
 endmodule
