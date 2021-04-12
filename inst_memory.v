91a0673b95ecc36f4303df52cecb90d7939078a1
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-12 00:35:29 -0700

    Update inst_memory.v
    formatting

--- inst_memory.v
+++ inst_memory.v
@@ -1,13 +1,12 @@
 module inst_memory(input [31:0]  address,
-            	   output [31:0] read_data);
+                   output [31:0] read_data);
+    
+    reg [31:0] RAM[63:0];
 
-  reg [31:0] RAM[63:0];
+	initial begin
+		$readmemh("memfile.dat",RAM); // initialize memory with test program. 
+	end
 
-  initial
-    begin
-      $readmemh("memfile.dat",RAM); // initialize memory with test program
-    end
-
-            assign read_data = RAM[address[7:2]]; // word aligned
+	assign read_data = RAM[address[7:2]]; // word aligned
 
 endmodule
