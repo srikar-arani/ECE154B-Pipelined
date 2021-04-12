9057f02ac2bf2c8062c8662a0a9b21f6d733ccb4
Srikar Arani <58278617+srikar-arani@users.noreply.github.com>
2021-04-11 20:40:37 -0700

    Updated Controller and Testbench

--- controller.v
+++ controller.v
@@ -44,37 +44,38 @@ module maindec(input  [5:0] op, func,
 
   assign {memwrite, memread, regwrite, alusrcA, alusrcB, se_ze, regdst, start_mult, mult_sign, memtoreg, out_select, alu_op} = controls;
 
-  // [TODO:] INSERT ACTUAL CONTROL SIGNALS
   // [TODO:] Maybe add output_branch signal
 
   always @ * begin
     case (op)
-      6'b001000: controls <= 16'b0x1xxx1000000010; // ADDI
-      6'b001001: controls <= 16'b0x1xxx1000000010; // ADDIU
-      6'b001100: controls <= 16'b0x1xxx1000000010; // ANDI
-      6'b001101: controls <= 16'b0x1xxx1000000010; // ORI
-      6'b001110: controls <= 16'b0x1xxx1000000010; // XORI
-      6'b001010: controls <= 16'b0x1xxx1000000010; // SLTI
-      6'b001001: controls <= 16'b0x1xxx1000000010; // SLTIU
-      6'b100011: controls <= 16'b0x1xxx1000000010; // LW
-      6'b101011: controls <= 16'b0x1xxx1000000010; // SW
-      6'b001111: controls <= 16'b0x1xxx1000000010; // LUI
-      6'b000010: controls <= 16'b0x1xxx1000000010; // J
-      6'b000101: controls <= 16'b0x1xxx1000000010; // BNE
-      6'b000100: controls <= 16'b0x1xxx1000000010; // BEQ
+      6'b001000: controls <= 16'b0010100000000010; // ADDI
+      6'b001001: controls <= 16'b0010100000000010; // ADDIU
+      6'b001100: controls <= 16'b0010110000000000; // ANDI
+      6'b001101: controls <= 16'b0010110000000001; // ORI
+      6'b001110: controls <= 16'b0010110000000100; // XORI
+      6'b001010: controls <= 16'b0010100000001011; // SLTI
+      6'b001011: controls <= 16'b0010100000001011; // SLTIU
+      6'b100011: controls <= 16'b0010100001000010; // LW
+      6'b101011: controls <= 16'b1000100000000010; // SW
+      6'b001111: controls <= 16'b0010000000010000; // LUI
+      6'b000010: controls <= 16'b0000000000000000; // J
+      6'b000101: controls <= 16'b0000000000001010; // BNE
+      6'b000100: controls <= 16'b0000000000001010; // BEQ
       6'b000000: case(func)                        // RTYPE
-	6'b100000: controls <= 16'b0x1xxx1000000010; // ADD
-	6'b100001: controls <= 16'b0x1xxx1000000010; // ADDU
-	6'b100010: controls <= 16'b0x1xxx1000000010; // SUB
-	6'b100011: controls <= 16'b0x1xxx1000000010; // SUBU
-	6'b100100: controls <= 16'b0x1xxx1000000010; // AND
-	6'b100101: controls <= 16'b0x1xxx1000000010; // OR
-	6'b100110: controls <= 16'b0x1xxx1000000010; // XOR
-	6'bxxxxxx: controls <= 16'b0x1xxx1000000010; // XNOR ?????????????
-	6'b101010: controls <= 16'b0x1xxx1000000010; // SLT
-	6'b101001: controls <= 16'b0x1xxx1000000010; // SLTU
-	6'b011000: controls <= 16'b0x1xxx1000000010; // MULT
-	6'b011001: controls <= 16'b0x1xxx1000000010; // MULTU
+	6'b100000: controls <= 16'b0010001000000010; // ADD
+	6'b100001: controls <= 16'b0010001000000010; // ADDU
+	6'b100010: controls <= 16'b0010001000001010; // SUB
+	6'b100011: controls <= 16'b0010001000001010; // SUBU
+	6'b100100: controls <= 16'b0010001000000000; // AND
+	6'b100101: controls <= 16'b0010001000000001; // OR
+	6'b100110: controls <= 16'b0010001000000100; // XOR
+	6'b011111: controls <= 16'b0010001000000101; // XNOR Use available opcode 011111
+	6'b101010: controls <= 16'b0010001000001011; // SLT
+	6'b101001: controls <= 16'b0010001000001011; // SLTU
+	6'b011000: controls <= 16'b0000000110000000; // MULT
+	6'b011001: controls <= 16'b0000000100000000; // MULTU
+	6'b010000: controls <= 16'b0010001000100000; // MFHI
+	6'b010010: controls <= 16'b0010001000110000; // MFLO
       endcase
       default: controls <= 16'bxxxxxxxxxxxxxxxx;     // illegal op
     endcase
--- controller_tb.v
+++ controller_tb.v
@@ -1,1 +1,95 @@
-//[TODO:] This all needs to be added, not sure how to go about it as its got finnicky ports
+module controller_tb;
+  
+  reg [5:0] op, func;
+  reg eq_ne;
+  wire memwrite, memread;
+  wire regwrite;
+  wire alusrcA, alusrcB;
+  wire se_ze, regdst;
+  wire start_mult, mult_sign;
+  wire memtoreg;
+  wire [1:0] pc_source, out_select;
+  wire [3:0] alu_op;
+  wire output_branch;
+
+  controller controller_test (
+    .op(op),
+    .func(func),
+    .eq_ne(eq_ne),
+    .memwrite(memwrite),
+    .memread(memread),
+    .regwrite(regwrite),
+    .alusrcA(alusrcA),
+    .alusrcB(alusrcB),
+    .se_ze(se_ze),
+    .regdst(regdst),
+    .start_mult(start_mult),
+    .mult_sign(mult_sign),
+    .memtoreg(memtoreg),
+    .pc_source(pc_source),
+    .out_select(out_select),
+    .alu_op(alu_op),
+    .output_branch(output_branch)
+  );
+
+  initial
+    begin
+      assign op = 6'b000000;
+      assign func = 6'h20;
+      assign eq_ne = 0;
+      #20;
+      assign func = 6'h21;
+      #20;
+      assign func = 6'h22;
+      #20;
+      assign func = 6'h23;
+      #20;
+      assign func = 6'h24;
+      #20;
+      assign func = 6'h25;
+      #20;
+      assign func = 6'h26;
+      #20;
+      assign func = 6'h1f;
+      #20;
+      assign func = 6'h2a;
+      #20;
+      assign func = 6'h2b;
+      #20;
+      assign func = 6'h18;
+      #20;
+      assign func = 6'h19;
+      #20;
+      assign func = 6'h10;
+      #20;
+      assign func = 6'h12;
+      #20;
+      assign op = 6'h8;
+      #20;
+      assign op = 6'h9;
+      #20;
+      assign op = 6'hc;
+      #20;
+      assign op = 6'hd;
+      #20;
+      assign op = 6'he;
+      #20;
+      assign op = 6'ha;
+      #20;
+      assign op = 6'hb;
+      #20;
+      assign op = 6'h23;
+      #20;
+      assign op = 6'h2b;
+      #20;
+      assign op = 6'hf;
+      #20;
+      assign op = 6'h2;
+      #20;
+      assign op = 6'h5;
+      #20;
+      assign op = 6'h4;
+      #20;
+    end
+
+endmodule
