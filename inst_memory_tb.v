85b7fd89abaedbefb2a64ff34483459302a0c6ac
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-11 18:56:36 -0700

    Update inst_memory_tb.v

--- inst_memory_tb.v
+++ inst_memory_tb.v
@@ -12,7 +12,7 @@ module inst_memory_tb();
 	initial begin
 		assign address = 8'h00000000;
 		#10;
-		if (read_data == 8'h00000000) begin
+		if (read_data == 8'h20020005) begin
 			$display("PASS");
 			#5;
 			$stop;
