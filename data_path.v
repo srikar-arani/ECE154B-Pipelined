9bb749ff625e7a84675337e0b120ae744f3889d1
lunacruz <lunacruz@umail.ucsb.edu>
2021-04-14 14:22:42 -0700

    Update data_path.v
    this is the version we used to perform checkoff

--- data_path.v
+++ data_path.v
@@ -1,11 +1,12 @@
 module datapath(input         clk, reset,
-                input         memreadD, regwriteD,
+                input         memreadD, regwriteD, memwriteD,
                 input         alusrcAD, alusrcBD,
                 input         se_zeD, regdstD,
 		input	      start_multD, mult_signD,
 		input	      memtoregD, output_branchD,
 		input  [1:0]  out_selectD,
                 input  [3:0]  alu_opD,
+		input  [1:0]  pc_sourceD,
                 output [31:0] pcF,
                 input  [31:0] instr,
                 output [31:0] aluoutM, writedataM,
@@ -13,7 +14,9 @@ module datapath(input         clk, reset,
 		output eq_ne);
 
   wire [4:0] writeregE, writeregM, RsE, RtE, RdE, writeregW;
-  wire [31:0] pcnext, pcnextbr, pcplus4F, pcplus4D, pcbranchD, instrD, pc_sourceD;
+  wire [31:0] pcnext, pcnextbr, pcplus4F, pcplus4D, pcbranchD, instrD /*, pc_sourceD*/;
+  wire [1:0] forwardAE, forwardBE;
+  wire forwardAD, forwardBD;
   wire [31:0] signimmD, signimmshD, signimmE;
   wire [31:0] zeroimmD, se_zeoutD, se_zeoutE, se_zeshE;
   wire [31:0] rd1, rd2, rd1E, rd2E, srcaE, writedataE, srcbE, multhi, multlo, multhighE, multlowE;
@@ -23,22 +26,27 @@ module datapath(input         clk, reset,
   wire [3:0] alu_opE;
   wire [63:0] multE;
   wire [31:0] equalAD, equalBD;
+  wire stallF, stallD;
+  wire flushE;
+  wire [32:0] pc_jump;
+  wire regwriteM, regwriteW;
+  
 
 
- hazard_detector hazards(RsD, RtD, RsE, RtE,
+ hazard_detector hazards(instr[25:21], instr[20:16], RsE, RtE,
                           writeregE, writeregM, writeregW,
                           memtoregE, memtoregM,
-                          RegWriteE, RegWriteM, RegWriteW,
-                          start_mult,
-                          PC_source,
-                          clk,
+                          regwriteE, regwriteM, regwriteW,
+                          start_multD,
+                          pc_sourceD,
+                          /*clk,*/
                           // counter,
                           stallF, stallD,
                           forwardAD, forwardBD,
                           flushE,
-                          forwardAE, forwardBE,
-                          start
-                          // counter_icr
+                          forwardAE, forwardBE
+                          /*start
+                          // counter_icr*/
                           );
 
 
@@ -49,31 +57,32 @@ module datapath(input         clk, reset,
 
 
   // next PC logic
-  flope #(32) pcreg(clk, ~stallF, pcnext, pcF);
+  flopre #(32) pcreg(clk, reset, ~stallF, pcnext, pcF);
   adder pcadd1(pcF, 32'b100, pcplus4F);
   sl2 immsh(signimmD, signimmshD);
   // double check if the pcplus4 on line below is D or F
-  adder pcadd2(pcplus4F, signimmshD, pcbranchD);
-  mux2 #(32) pcbrmux(pcplus4F, pcbranchD, pc_sourceD, pcnext);
-  flopfd #(32) fetch_decode(clk, pc_sourceD, ~stallD, instr, pcplus4F, instrD, pcplus4D);
+  adder pcadd2(pcplus4D, signimmshD, pcbranchD);
+  //sl2 jumpshift(instr[25:0], pc_jump);
+  mux3 #(32) pcbrmux(pcplus4F, pcbranchD, {pcplus4D[31:28],instr[25:0],2'b00},  pc_sourceD, pcnext);
+  flopfd #(32) fetch_decode(clk, (pc_sourceD[1]|pc_sourceD[0]), stallD, instr, pcplus4F, instrD, pcplus4D);
 
   // register file logic
-  reg_file rf(clk, regwriteW, reset, instrD[25:21], instr[20:16], writeregW, resultW, rd1, rd2);
-  signext se(instr[15:0], signimmD);
-  zeroext ze(instr[15:0], zeroimmD);
+  reg_file rf(clk, regwriteW, reset, instrD[25:21], instrD[20:16], writeregW, resultW, rd1, rd2);
+  signext se(instrD[15:0], signimmD);
+  zeroext ze(instrD[15:0], zeroimmD);
   mux2 #(32) se_zemux(signimmD, zeroimmD, se_zeD, se_zeoutD);
   flopde #(32) decode_execute(clk, flushE, memwriteD, regwriteD, alusrcBD, regdstD, start_multD, mult_signD, memtoregD,
 	       out_selectD,
 	       alu_opD,
 	       rd1, rd2, se_zeoutD,
-	       instr[25:21], instr[20:16], instr[15:11],
+	       instrD[25:21], instrD[20:16], instrD[15:11],
 	       rd1E, rd2E, se_zeoutE,
 	       RsE, RtE, RdE,
 	       memwriteE, regwriteE, alusrcBE, regdstE, start_multE, mult_signE, memtoregE,
 	       out_selectE,
 	       alu_opE);
 
-  mux2 #(32) dstmux(RtE, RdE, regdstE, writeregE);
+  mux2 #(5) dstmux(RtE, RdE, regdstE, writeregE);
   
   // ALU logic
   mux3 #(32) forwardmuxA(rd1E, resultW, aluoutM,forwardAE,srcaE);
