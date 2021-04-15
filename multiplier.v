96f1f45338803bc28b241414ddd5ce10546048cd
Venkat Krishnan <vkrishnan@umail.ucsb.edu>
2021-04-14 23:58:02 -0700

    mult

--- multiplier.v
+++ multiplier.v
@@ -1,19 +1,30 @@
-module multiplier(input  [31:0] a,b,
-		  input clk,start,is_signed,
-		  output [63:0] s);
+module multiplier(input [31:0] a, b,
+           input clk,
+           input start, is_signed,
+           output  [63:0] s);
 
-  //value to hold result of multiplication
-  reg [63:0] M;
+  wire [31:0] AA;
+  assign AA = (a[31]) ? (~a+a[31]) : a ;
+  wire [31:0] BB;
+  assign BB = (b[31]) ? (~b+b[31]) : b;
 
-  //multiplier Implementation
-  always @ (posedge clk) begin
-    case((is_signed & start))
-      //lab says to assume no difference between signed and unsigned instructions
-      1'b0: M <= M;
-      1'b1: M <= a * b;
-    endcase
+  reg [63:0] product;
+
+  always @(posedge clk) begin
+    if(start) begin
+      if(is_signed) begin
+        if ((a[31]&~b[31]) || (~a[31]&b[31])) begin
+          product <= -1*(AA*BB);
+        end else begin
+          product <= AA*BB;
+        end
+      end else begin
+        product <= a*b;
+      end
+
+    end
   end
 
-  assign s = M;
+  assign s = product;
 
 endmodule
